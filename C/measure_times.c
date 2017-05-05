5dfcb9bc9eb9f85beb49a77e474c4b58e8af3fa0
Roberto Esposito <roberto.esposito@unito.it>
2017-05-05 18:00:00 +0200

    Support for comparing std library sorting functions

--- C/measure_times.c
+++ C/measure_times.c
@@ -24,15 +24,59 @@ void PrintTime_print(char* msg, void (^callback)(void)) {
   printf("Elapsed time:%lf\n", (end - start) / (double)CLOCKS_PER_SEC);
 }
 
+// qsort passes to the comparison function a pointer to the array element,
+// not the element itself. Dataset_compare_field? assumes that the given
+// elements are actually the array elements themeselves. We need to dereference
+// the pointers to make the things agree.
+static int qsort_compare_field1(const void* e1, const void* e2) {
+  return Dataset_compare_field1( *( void* const*)e1, *(void* const*)e2 );
+}
 
-static void test_algorithm(Dataset* input_dataset, void (*sort)(void**, size_t, int(*)(const void*, const void*))) {
+static int qsort_compare_field2(const void* e1, const void* e2) {
+  return Dataset_compare_field2( *(void* const*)e1, *(void* const*)e2 );
+}
+
+static int qsort_compare_field3(const void* e1, const void* e2) {
+  return Dataset_compare_field3( *(void* const*)e1, *(void* const*)e2 );
+}
+
+
+static void test_qsort(Dataset* input_dataset) {
+  // exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
+  //   PrintTime_print("field1", ^{
+  //     printf("Sorting according to field1\n");
+  //     qsort((void**) Array_carray(dataset), Array_size(dataset), sizeof(Record*), qsort_compare_field1);
+  //     printf("Done!\n");
+  //   });
+  // });
+
   exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
-    PrintTime_print("field1", ^{
-      printf("Sorting according to field1\n");
-      sort((void**)Array_carray(dataset), Array_size(dataset), Dataset_compare_field1);
+    PrintTime_print("field2", ^{
+      printf("Sorting according to field2\n");
+      qsort((void**) Array_carray(dataset), Array_size(dataset), sizeof(Record*), qsort_compare_field2);
       printf("Done!\n");
     });
   });
+
+  // exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
+  //   PrintTime_print(pt, "field3", ^{
+  //     printf("Sorting according to field3\n");
+  //     qsort((void**) Array_carray(dataset), Array_size(dataset), sizeof(Record*), qsort_compare_field3);
+  //     printf("Done!\n");
+  //   });
+  // });
+
+}
+
+
+static void test_algorithm(Dataset* input_dataset, void (*sort)(void**, size_t, int(*)(const void*, const void*))) {
+  // exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
+  //   PrintTime_print("field1", ^{
+  //     printf("Sorting according to field1\n");
+  //     sort((void**)Array_carray(dataset), Array_size(dataset), Dataset_compare_field1);
+  //     printf("Done!\n");
+  //   });
+  // });
   exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
     PrintTime_print("field2", ^{
       printf("Sorting according to field2\n");
@@ -40,19 +84,20 @@ static void test_algorithm(Dataset* input_dataset, void (*sort)(void**, size_t, 
       printf("Done!\n");
     });
   });
-  exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
-    PrintTime_print("field3", ^{
-      printf("Sorting according to field3\n");
-      sort((void**)Array_carray(dataset), Array_size(dataset), Dataset_compare_field3);
-      printf("Done!\n");
-    });
-  });
+  // exec_and_print_with_dup_storage(input_dataset, ^(Array* dataset) {
+  //   PrintTime_print("field3", ^{
+  //     printf("Sorting according to field3\n");
+  //     sort((void**)Array_carray(dataset), Array_size(dataset), Dataset_compare_field3);
+  //     printf("Done!\n");
+  //   });
+  // });
 }
 
 
 static void print_usage() {
   printf("Usage: measure_time <opt> <file name>\n");
   printf(" opts: -q use quick_sort algorithm\n");
+  printf("       -Q use stdlib qsort algorithm\n");
   printf("       -h print this message\n");
 }
 
@@ -71,7 +116,7 @@ static void check_arguments(int argc, char** argv) {
     exit(ERROR_ARGUMENT_PARSING);
   }
 
-  if(strlen(argv[1])!=2 || argv[1][0] != '-' || !char_included(argv[1][1], (char[]){'q','h'}, 2)) {
+  if(strlen(argv[1])!=2 || argv[1][0] != '-' || !char_included(argv[1][1], (char[]){'q','h', 'Q'}, 3)) {
     printf("Option %s not recognized\n", argv[1]);
     print_usage();
     exit(ERROR_ARGUMENT_PARSING);
@@ -99,6 +144,9 @@ int main(int argc, char* argv[]) {
   switch(argv[1][1]) {
     case 'q':
       test_algorithm(dataset, quick_sort);
+      break;
+    case 'Q':
+      test_qsort(dataset);
       break;
     default:
       assert(0); // should never get here
